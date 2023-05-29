32b2ad998253a4759b3463f50e8866c8aeda1f7b
brito1212 <felipef_brito@outlook.com>
2023-05-29 20:09:17 -0300

    feat: scheduler com dequeue por prioridade

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,23 +6,6 @@
 #define MAX_QUEUE_SIZE 10
 #define MAX_NUM_QUEUES 10
 
-// typedef struct Process{
-//  int id;
-//  int priority;
-//  int time_left;
-//  int running;
-// } Process;
-
-// typedef struct {
-//     Process * processes[MAX_QUEUE_SIZE];
-//     int head;
-//     int tail;
-// } Queue;
-
-// typedef struct {
-//     Queue queues[MAX_NUM_QUEUES];
-//     int num_queues;
-// } Scheduler;
 
 void scheduler_init(Scheduler *s) {
     s->num_queues = 0;
@@ -43,6 +26,7 @@ int enqueue(Queue *q, Process *item) {
     if ((q->tail + 1) % MAX_QUEUE_SIZE == q->head) {
         return -1; // queue is full
     }
+    item->enqueued = 1;
     q->processes[q->tail] = item;
     q->tail = (q->tail + 1) % MAX_QUEUE_SIZE;
     return 0;
@@ -65,7 +49,7 @@ void *dequeue_by_priority(Queue *q){
     int remove_idx = q->head;
     int j = (q->head+1) % MAX_QUEUE_SIZE;
     while(j!=q->tail){
-        if (q->processes[j]->priority < q->processes[remove_idx]->priority){
+        if (q->processes[j]->priority > q->processes[remove_idx]->priority){
             remove_idx = j;
         }
         j = (j+1)%MAX_QUEUE_SIZE;
@@ -73,33 +57,11 @@ void *dequeue_by_priority(Queue *q){
     Process * swap = q->processes[q->head];
     q->processes[q->head] = q->processes[remove_idx];
     q->processes[remove_idx] = swap;
-    void *item = q->processes[q->head];
+    Process *item = q->processes[q->head];
     q->head = (q->head +1) % MAX_QUEUE_SIZE;
     
     return item;
 }
-
-// void *dequeue_by_time_left(Queue *q){
-//     if (q->head == q->tail) {
-//         return NULL; // queue is empty
-//     }
-    
-//     int remove_idx = q->head;
-//     int j = (q->head+1) % MAX_QUEUE_SIZE;
-//     while(j!=q->tail){
-//         if (q->processes[j]->time_left < q->processes[remove_idx]->time_left){
-//             remove_idx = j;
-//         }
-//         j = (j+1)%MAX_QUEUE_SIZE;
-//     }
-//     Process * swap = q->processes[q->head];
-//     q->processes[q->head] = q->processes[remove_idx];
-//     q->processes[remove_idx] = swap;
-//     void *item = q->processes[q->head];
-//     q->head = (q->head +1) % MAX_QUEUE_SIZE;
-    
-//     return item;
-// }
 
 int scheduler_enqueue(Scheduler *s, int queue_idx, Process *item) {
     if (queue_idx < 0 || queue_idx >= s->num_queues) {
@@ -108,11 +70,16 @@ int scheduler_enqueue(Scheduler *s, int queue_idx, Process *item) {
     return enqueue(&(s->queues[queue_idx]), item);
 }
 
-void *scheduler_dequeue(Scheduler *s) {
+void *scheduler_dequeue(Scheduler *s, int dequeue_type) {
     int i;
     for (i = 0; i < s->num_queues; i++) {
         Queue *q = &(s->queues[i]);
-        void *item = dequeue(q);
+        void *item;
+        if(dequeue_type == 0){
+            item = dequeue_by_priority(q);
+        }else {
+            item = dequeue(q);   
+        }
         if (item != NULL) {
             return item;
         }
